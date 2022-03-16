158cc296a8acb0f5261b70e0fd3aed82c1cabe95
Muhammadali-Akbarov <muhammadali17abc@gmail.com>
2022-03-16 11:12:47 +0500

    DB events

---
+++ README.md
@@ -0,0 +1,18 @@
+# My Taxi Email Api v1 
+Sourcode and Resources for My Taxi Email Api v1
+
+# Installation
+* 1 - clone repo https://git.mytaxi.uz/backend/email_api
+* 2 - create a virtual environment and activate
+*  - pip install virtualenv
+*  - virtualenv envname
+*  - source envname\scripts\activate
+* 3 - cd into project "cd email_api"
+* 4 - pip install -r requirements.txt
+* 5 - python manage.py runserver
+
+# Features
+* POST method for send message to client
+
+# About more
+https://newwiki.mytaxi.uz/ru/services/email_api
---
+++ email_api/api/views.py
@@ -0,0 +1,45 @@
+import logging
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from core.libraries.sys_params import SysParams as SystemParams
+from django.core.mail import send_mail
+
+from email_api.models import SendMessage
+
+sys_params = SystemParams()
+logger = logging.getLogger(__name__)
+
+
+@api_view(['POST'])
+def sendEmail(request):
+    email_to = request.data['email_to']
+    mess_title = request.data['mess_title']
+    mess_body = request.data['mess_body']
+    from_to = request.data['from_to']
+    
+    if request.method == "POST":
+    
+        send_mail(
+            mess_title,
+            mess_body,
+            from_to,
+            [email_to],
+            fail_silently=False,
+        )
+        
+        message = SendMessage.objects.create(
+            email = email_to,
+            mess_title = mess_title,
+            mess_body = mess_body,
+            from_to = from_to,
+            status = 1
+        )
+        if message != None:
+            message.save()
+            
+        return Response({"message": "Email has been sent successfully"}, status=status.HTTP_200_OK)
+
+    return Response({"message": "method not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
